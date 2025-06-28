009e245e19226ed4727fd1e89c5ec6bb908e7bba
ABrain-One <AI-Linux@ABrain.one>
2025-06-28 11:34:03 +0200

    Support all CLI arguments passed to run.py

--- run50.sh
+++ run50.sh
@@ -3,8 +3,4 @@
 # An endless training loop, restarting the program after critical errors.
 # All image classification tasks are randomly shuffled and trained over 50 epochs.
 
-if [ -d .venv ]; then
-    source .venv/bin/activate
-fi
-
-while true ; do python run.py -c img-classification -e 50 -r 1; done
+bash run.sh -c img-classification -e 50 -r 1
