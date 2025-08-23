a4e1dbdbc861064c6c4f9d5c2d50f5f478c70086
KrunalJesani <krunaljesani@gmail.com>
2025-08-23 14:05:00 +0200

    NN-statistics

--- ab/nn/nn/ConvNeXtTransformer_ALTER_2.py
+++
@@ -1,8 +0,0 @@
-self.decoder = TransformerDecoder(
-    vocab_size=vocab_size,
-    d_model=d_model,
-    num_layers=num_layers,
-    nhead=nhead,  # Change nhead here
-    dropout=dropout,
-    tie_weights=tie_weights
-)
